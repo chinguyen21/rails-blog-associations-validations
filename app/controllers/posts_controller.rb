7a2f98081c19b58efa2d27d510dcac1f0e33039d
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-23 17:19:20 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,10 +24,15 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(name: post_params[:name], content: post_params[:content], user_id: post_params[:user_id])
 
     respond_to do |format|
       if @post.save
+        post_params[:tags].each do |id|
+          if id != ""
+            PostTag.create(tag_id: id, post_id: @post.id)
+          end
+        end
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
@@ -41,7 +46,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
+      if @post.update(name: post_params[:name], content: post_params[:content], user_id: post_params[:user_id])
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
@@ -69,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, :user_id, tags: [])
     end
 end
